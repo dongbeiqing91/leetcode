d48ab2f79d5c697dc554308079fba89f73f80eda
bedong <bedong@ebay.com>
2025-07-03 21:55:48 +0800

    isPalindrome

---
+++ easy/isPalindrome.go
@@ -0,0 +1,48 @@
+package easy
+
+import (
+	"strconv"
+)
+
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+	if x < 10 {
+		return true
+	}
+	str := strconv.Itoa(x)
+	l := 0
+	r := len(str) - 1
+	for l <= r {
+		if str[l] != str[r] {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+// 官方解法
+func isPalindrome2(x int) bool {
+	// 特殊情况：
+	// 如上所述，当 x < 0 时，x 不是回文数。
+	// 同样地，如果数字的最后一位是 0，为了使该数字为回文，
+	// 则其第一位数字也应该是 0
+	// 只有 0 满足这一属性
+	if x < 0 || (x%10 == 0 && x != 0) {
+		return false
+	}
+
+	revertedNumber := 0
+	for x > revertedNumber {
+		revertedNumber = revertedNumber*10 + x%10
+		x /= 10
+	}
+
+	// 当数字长度为奇数时，我们可以通过 revertedNumber/10 去除处于中位的数字。
+	// 例如，当输入为 12321 时，在 while 循环的末尾我们可以得到 x = 12，revertedNumber = 123，
+	// 由于处于中位的数字不影响回文（它总是与自己相等），所以我们可以简单地将其去除。
+	return x == revertedNumber || x == revertedNumber/10
+}
